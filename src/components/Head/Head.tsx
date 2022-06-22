fd83b565dfb0eee98980d50c033e985f969ec824
Maurice Murphy <mauricemurphy@Maurices-MacBook-Pro.local>
2022-06-22 16:32:03 +0100

    feat: add next/head meta data component

---
+++ src/components/Head/Head.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+type MetaProps = {
+  page: string;
+};
+
+const Meta = ({ page }: MetaProps) => {
+  return (
+    <Head>
+      <title>{`Fabrique ECommerce - ${page}`}</title>
+      <meta name="description" content={`Fabrique ECommerce - ${page}`} />
+      <meta
+        property="og:title"
+        content={`Fabrique ECommerce - ${page}`}
+        key="title"
+      />
+    </Head>
+  );
+};
+
+export default Meta;
